ccaffbd901232024b701ac5b9f52f9058aa5937d
Olivier <o.blaivie@coperbee.fr>
2022-07-19 14:00:26 +0200

    React v17 to 18 correctifs !

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom'
 import './App.css';
+import Login from './pages/login'
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path='/'>
+          <Route index element={<Login />} />
+          <Route path='login' element={<Login />} />
+        </Route>
+      </Routes>
     </div>
-  );
+  )
 }
 
 export default App;
